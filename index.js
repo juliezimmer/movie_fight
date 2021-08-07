aadf73d68463dd08a04e26cc7727fdd57d68699d
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-08-07 16:57:13 -0500

    created two get requests with axios to omdbapi for movie info

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-// network, server request using axios  
+// network, server request using axios  to fetch movie data //
+const fetchData = async () => { // helper function
+   const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+         apikey: '90d486a',
+         i: 'tt0848228' 
+      }
+   });
+   console.log(response.data);
+};
+
+fetchData();
 
